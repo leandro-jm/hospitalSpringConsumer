3583ff86a9f4f6fede860aaa34568cf1009514ce
leandro.jm <leandro.jm@gmail.com>
2023-05-24 23:31:20 -0300

    First commit.

---
+++ src/main/java/br/com/ljm/hospitalspringconsumer/HospitalSpringConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ljm.hospitalspringconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSpringConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ljm/hospitalspringconsumer/HospitalSpringConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ljm.hospitalspringconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
